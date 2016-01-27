fa8c9a0b3fe6c91c97251eb6d3fe2f39e81e6fe5
Phill <phill@starkingdom.co.uk>
2016-01-27 21:23:24 +0000

    Fixing Imported function loading

--- R/b2AuthorizeAccount.R
+++ R/b2AuthorizeAccount.R
@@ -59,7 +59,7 @@ b2AuthorizeAccount <- function(url, accountId, authorizationKey) {
 
   # Check for bad authorisation and sent message
   if (httr::status_code(b2Return) != "200") {
-    badReturn <- jsonlite::fromJSON(content(b2Return,type = "text"))
+    badReturn <- jsonlite::fromJSON(httr::content(b2Return,type = "text"))
     stop(
       "Status Code: ", badReturn$code, "\n Message: ", badReturn$message, "\nPlease check your account ID and Authorisation Key. Remember, a new Authorisation key is generated every time you click 'Create application key' in the B2 web interface. \n"
     )
@@ -67,7 +67,7 @@ b2AuthorizeAccount <- function(url, accountId, authorizationKey) {
   } else {
     # Output as dataframe. Global variable, ooooohh
     accountAuthorization <<-
-      as.data.frame(jsonlite::fromJSON(content(b2Return, type = "text")))
+      as.data.frame(jsonlite::fromJSON(httr::content(b2Return, type = "text")))
   }
 }
 
